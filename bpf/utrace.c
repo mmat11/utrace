221fa918167ea8c102a5f78df20870ec34e1d67e
Mattia Meleleo <mattia.meleleo@elastic.co>
2022-02-11 00:59:31 +0100

    Format C

--- bpf/utrace.c
+++ bpf/utrace.c
@@ -8,43 +8,43 @@ struct {
 
 enum ev_kind {
     EV_KIND_ENTRY = 1,
-    EV_KIND_EXIT  = 2,
+    EV_KIND_EXIT = 2,
 };
 
 struct event {
     enum ev_kind kind;
-	u64 pid_tgid;
+    u64 pid_tgid;
     u64 ts;
     u64 cookie;
 } __attribute__((packed));
 
 static void emit_event(enum ev_kind kind, u64 cookie) {
-	struct event *ev = bpf_ringbuf_reserve(&ringbuf, sizeof(*ev), 0);
+    struct event *ev = bpf_ringbuf_reserve(&ringbuf, sizeof(*ev), 0);
     if (!ev) {
         bpf_printk("ringbuf reserve failed");
         return;
     }
 
-	ev->pid_tgid = bpf_get_current_pid_tgid();
+    ev->pid_tgid = bpf_get_current_pid_tgid();
     ev->ts = bpf_ktime_get_ns();
     ev->kind = kind;
     ev->cookie = cookie;
 
-	bpf_ringbuf_submit(ev, 0);
+    bpf_ringbuf_submit(ev, 0);
 }
 
 SEC("uprobe/generic")
 int uprobe_generic(struct pt_regs *ctx) {
     u64 cookie = bpf_get_attach_cookie(ctx);
     emit_event(EV_KIND_ENTRY, cookie);
-	return 0;
+    return 0;
 }
 
 SEC("uretprobe/generic")
 int uretprobe_generic(struct pt_regs *ctx) {
     u64 cookie = bpf_get_attach_cookie(ctx);
     emit_event(EV_KIND_EXIT, cookie);
-	return 0;
+    return 0;
 }
 
 char LICENSE[] SEC("license") = "Dual MIT/GPL";
